38a75d6fff8afbbea57babf0de17cd5edb7cab0a
schma41 <schma41@bfh.ch>
2025-12-19 21:45:32 +0100

    Add Leaflet

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,51 @@ async function loadDog() {
 
 document.getElementById("dogButton").addEventListener("click", loadDog);
 loadDog();
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const map = L.map("map").setView([46.8, 7.6], 9);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors",
+  }).addTo(map);
+
+
+  const bfhMarker = L.marker([46.9511, 7.4386]).addTo(map);
+
+  bfhMarker
+    .bindPopup("<strong>BFH Wirtschaft</strong><br>Bern")
+    .bindTooltip("BFH Wirtschaft", {
+      permanent: false,
+      direction: "top",
+      offset: [0, -10],
+    });
+
+
+  bfhMarker.on("mouseover", function () {
+    this.openTooltip();
+  });
+
+  bfhMarker.on("mouseout", function () {
+    this.closeTooltip();
+  });
+
+
+  const wimmisMarker = L.marker([46.6756, 7.6393]).addTo(map);
+
+  wimmisMarker
+    .bindPopup("<strong>Wohnort</strong><br>Wimmis")
+    .bindTooltip("Wohnort: Wimmis", {
+      permanent: false,
+      direction: "top",
+      offset: [0, -10],
+    });
+
+  wimmisMarker.on("mouseover", function () {
+    this.openTooltip();
+  });
+
+  wimmisMarker.on("mouseout", function () {
+    this.closeTooltip();
+  });
+});
